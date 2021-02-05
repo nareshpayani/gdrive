b2d9f889b856f883af1afab78e61e78e65924cad
nareshpayani <naresh.be29@gmail.com>
2021-02-05 23:08:59 +0530

    Code Cleanup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJ1LglSm1_almuCt621kCB-hKiuqqIiwA",
+    authDomain: "drive-c9222.firebaseapp.com",
+    projectId: "drive-c9222",
+    storageBucket: "drive-c9222.appspot.com",
+    messagingSenderId: "157883339244",
+    appId: "1:157883339244:web:3c3dce17f6f33aa8cfb0c9",
+    measurementId: "G-KYT01LFKXC"
+  };
